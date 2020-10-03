959e08e1cb53cf51f72fc15d5448ad2fe1fd9f7f
amohamed1 <amohamed@farmdrive.co.ke>
2020-10-03 16:36:41 +0300

    Addition of login and sign up modal

--- js/main.js
+++ js/main.js
@@ -8,15 +8,53 @@ jQuery(document).ready(function($) {
           var modBtn  = $('#modBtn'),
               modal   = $('#modal'),
               close   = modal.find('.close-btn img'),
-              modContent = modal.find('.modal-content');
+              modContent = modal.find('.modal-content'),
+              likeBtn = $('#like-btn'),
+              loginBtn = $('#login-btn'),
+              loginModal = $('#login-modal'),
+              loginClose = loginModal.find('.close-btn img'),
+              loginModContent = loginModal.find('.modal-content'),
+              signUpBtn = $('#sign-up-btn'),
+              signUpModal = $('#sign-up-modal'),
+              signUpClose = signUpModal.find('.close-btn img'),
+              signUpModCOntent = signUpModal.find('.modal-content'),
+              signUpModLoginBtn = $('#sign-up-login-btn');
           
           // open modal when click on open modal button 
           modBtn.on('click', function() {
             modal.css('display', 'block');
             modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
           });
-          
-          // close modal when click on close button or somewhere out the modal content 
+
+          // open login modal when click on open modal button
+          likeBtn.on('click', function() {
+            loginModal.css('display', 'block');
+            loginModContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+          });
+
+          // open login modal when click on open modal button
+          loginBtn.on('click', function() {
+            loginModal.css('display', 'block');
+            loginModContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+          });
+
+          // open sign up modal when click on open modal button
+          signUpBtn.on('click', function() {
+            loginModContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+            signUpModal.css('display', 'block');
+            signUpModCOntent.removeClass('modal-animated-out').addClass('modal-animated-in');
+            loginModal.css('display', 'none');
+          });
+
+          // open login modal when click on open modal button
+          signUpModLoginBtn.on('click', function() {
+            loginModal.css('display', 'block');
+            loginModContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+            signUpModCOntent.removeClass('modal-animated-out').addClass('modal-animated-in');
+            signUpModCOntent.css('display', 'none');
+          });
+
+          // close modal when click on close button or somewhere out the modal content
           $(document).on('click', function(e) {
             var target = $(e.target);
             if(target.is(modal) || target.is(close)) {
@@ -26,8 +64,30 @@ jQuery(document).ready(function($) {
               });
             }
           });
-          
+
+          // close modal when click on close button or somewhere out the modal content
+          $(document).on('click', function(e) {
+            var target = $(e.target);
+            if(target.is(signUpModal) || target.is(signUpClose)) {
+              signUpModCOntent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                signUpModal.css('display', 'none');
+                next();
+              });
+            }
+          });
+
+          // close modal when click on close button or somewhere out the modal content
+          $(document).on('click', function(e) {
+            var target = $(e.target);
+            if(target.is(loginModal) || target.is(loginClose)) {
+              loginModContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                loginModal.css('display', 'none');
+                next();
+              });
+            }
+          });
         });
+
 
         // on click event on all anchors with a class of scrollTo
         $('a.scrollTo').on('click', function(){
