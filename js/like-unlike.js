7225025d0a92b58a5ddceeb58d9e223fd9bbc3f3
amohamed1 <amohamed@farmdrive.co.ke>
2020-10-05 23:32:16 +0300

    like-unlike script

--- js/like-unlike.js
+++ js/like-unlike.js
@@ -1,17 +1,100 @@
 jQuery(document).ready(function($) {
-
-    'use strict';
+    // window.localStorage.removeItem('buttonStatus')
     $(function() {
-        var likeBtn = $('.fa-heart');
+        var savedButtonStatus = window.localStorage.getItem('buttonStatus')
+        var buttonStatus = JSON.parse(savedButtonStatus);
+        console.log('savedButtonStatus ' + savedButtonStatus)
+        console.log('savedButtonStatus json ' + JSON.parse(savedButtonStatus))
+        if(jQuery.isEmptyObject(savedButtonStatus) === false){
+            console.log('buttonStatus ' + buttonStatus)
+            $.each(JSON.parse(savedButtonStatus), function (index, value){
+                if(value==='true'){
+                    console.log(index)
+                    console.log(document.getElementById(index).hasAttribute('class'))
+                    if(document.getElementById(index).hasAttribute('class')){
+                        document.getElementById(index).removeAttribute('class');
+                        document.getElementById(index).setAttribute('class', 'fas fa-heart');
+                    }
+                }
+            });
+        }
 
-        likeBtn.on('click', function (){
+        // window.localStorage.removeItem('buttonStatus')
+    })
+        'use strict';
+        var savedButtonStatus = window.localStorage.getItem('buttonStatus');
+        if(jQuery.isEmptyObject(savedButtonStatus) === false){
+            var buttonStatus = JSON.parse(savedButtonStatus);
+        }
+        else {
+            var buttonStatus = {}
+        }
+
+
+        $('.far').on('click', function (){
+            var likeBtn = $(this);
+            var imageId = $(this).attr('id')
+            let token = localStorage.getItem('token');
+            console.log(token)
+            var data = {}
+            data['email'] = window.localStorage.getItem('email')
+            data['item'] = imageId
+
+            console.log(data)
+
             if(likeBtn.hasClass("far")){
-                likeBtn.removeClass("far").addClass("fas")
+                likeBtn.removeClass("far").addClass("fas");
+                buttonStatus[imageId] = 'true'
+
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json",
+                    url: "https://cors-anywhere.herokuapp.com/https://2112c89ccd2b.ngrok.io/items/user-item/add",
+                    data: JSON.stringify(data),
+                    dataType: 'json',
+                    timeout: 8000000,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+                    },
+                    success: function (result) {
+
+                    },
+                    error: function (e) {
+                        console.log("ERROR: ", e);
+                        // display(e);
+                    }
+                });
             }
             else{
-                likeBtn.removeClass("fas").addClass("far")
-            }
-        })
+                likeBtn.removeClass("fas").addClass("far");
+                buttonStatus[imageId] = 'false'
+                $.ajax({
+                    type: "DELETE",
+                    contentType: "application/json",
+                    url: "https://cors-anywhere.herokuapp.com/https://2112c89ccd2b.ngrok.io/items/user-item/remove",
+                    data: JSON.stringify(data),
+                    dataType: 'json',
+                    timeout: 8000000,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+                    },
+                    success: function (result) {
+                        var status = result['status'];
+                        if(status === "success"){
 
-    })
+                        }
+                        else{
+                            alert(result['message']);
+                        }
+
+                    },
+                    error: function (e) {
+                        console.log("ERROR: ", e);
+                        display(e);
+                    }
+                });
+            }
+            window.localStorage.setItem('buttonStatus', JSON.stringify(buttonStatus))
+            console.log(window.localStorage.getItem('buttonStatus'))
+        })
 });
