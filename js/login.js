0190c99647f8f8e008936fcd1186ba21de331299
amohamed1 <amohamed@farmdrive.co.ke>
2020-10-05 23:38:08 +0300

    local storage for variables

--- js/login.js
+++ js/login.js
@@ -17,13 +17,17 @@ jQuery(document).ready(function($) {
             $.ajax({
                 type: "POST",
                 contentType: "application/json",
-                url: "https://cors-anywhere.herokuapp.com/https://bb94e7331667.ngrok.io/auth/login",
+                url: "https://cors-anywhere.herokuapp.com/https://2112c89ccd2b.ngrok.io/auth/login",
                 data: JSON.stringify(data),
                 dataType: 'json',
-                timeout: 800000,
+                timeout: 600000,
                 success: function (result) {
                     var status = result['status'];
                     if(status === "success"){
+                        localStorage.clear();
+                        localStorage.setItem('token', result['auth_token'])
+                        localStorage.setItem('email', userEmail)
+                        console.log(result['auth_token'])
                         window.location.href = "main.html";
                     }
                     else{
